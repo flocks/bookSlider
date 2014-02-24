9129bd5cbbec4a56c7db6869a3cd2fb18115e000
Florent Teissier <teissierflorent@gmail.com>
2014-02-24 14:53:51 +0100

    init plugin

--- src/bookSlider.js
+++ src/bookSlider.js
@@ -6,20 +6,33 @@
       
       var options = $.extend({
         "rotationSpeed" : 2,
-        "rotationAngle": 45
+        "rotationAngle": 2
       }, options);
       
+
+      var _init = function(list) {
+
+        for(var i = 0; i < list.length; i++) {
+          var val= i * -10;
+         
+          $(list[i]).css({
+            WebkitTransform: "rotateY("+val+"deg)",
+            transform:  "rotateY("+ val+"deg)",
+            MozTransform:  "rotateY("+val+ "deg)"
+          });
+        }
+      };
       
        return this.each(function(key, bookSlider) {
-        
+        _init($(bookSlider).find('li'));
+
         $(bookSlider).find('li').each(function(key, val) {
           $(val).on('click', function() {
-            console.log("la");
+
             $(val).css({
              transform :"rotateY(-180deg)"
             });
 
-            console.log($(val).css('transform'));
           });
         });
        });
